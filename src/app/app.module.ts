19342ff23faed1d68255bbd76cd5f8ce8697c628
Alessandro Ciccarone <ale70029@gmail.com>
2025-05-04 12:26:56 +0200

    shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,21 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
 import { TasksComponent } from "./tasks/tasks.component";
-import { CardComponent } from "./shared/card/card.component";
-import { FormsModule } from "@angular/forms";
-import { DatePipe } from "@angular/common";
 import { NewTaskComponent } from "./tasks/new-task/new-task.component";
+import { SharedModule } from "./shared/shared.module";
 
 @NgModule({
     declarations: [
         AppComponent,
         HeaderComponent,
         UserComponent,
-        CardComponent,
         TasksComponent, TasksComponent, NewTaskComponent], // non-standalone components or directives
     bootstrap: [AppComponent], //root component
-    imports: [BrowserModule, FormsModule] //standalone components or other modules
+    imports: [BrowserModule, FormsModule, SharedModule] //standalone components or other modules
 })
 export class AppModule { }
