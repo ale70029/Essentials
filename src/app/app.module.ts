b00b5f1c28f440213734bbb732b7a569bb4aba92
Alessandro Ciccarone <ale70029@gmail.com>
2025-05-04 12:32:26 +0200

    tasks module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,15 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
-import { TasksComponent } from "./tasks/tasks.component";
-import { NewTaskComponent } from "./tasks/new-task/new-task.component";
 import { SharedModule } from "./shared/shared.module";
+import { TasksModule } from "./tasks/tasks.module";
 
 @NgModule({
     declarations: [
         AppComponent,
         HeaderComponent,
-        UserComponent,
-        TasksComponent, TasksComponent, NewTaskComponent], // non-standalone components or directives
+        UserComponent], // non-standalone components or directives
     bootstrap: [AppComponent], //root component
-    imports: [BrowserModule, FormsModule, SharedModule] //standalone components or other modules
+    imports: [BrowserModule, SharedModule, TasksModule] //standalone components or other modules
 })
 export class AppModule { }
